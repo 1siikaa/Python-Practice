730e1811a3cba680a7a7536ef35bf7a82c2eb39e
Vanshika Dubey / <srishtidwivedi672@gmail.com>
2024-11-04 16:05:42 +0530

    learning-operators-part-1

---
+++ operators.py
@@ -0,0 +1,51 @@
+# arithmetic operators
+a = 2
+b = 5
+print(a+b) #addition
+print(a-b) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a%b) #modulus
+print(a**b) #exponentiation
+print(a//b) #floor division
+
+# assignment operator
+x=9
+x &=3 #bitwise and
+print(x)
+print(z:=3) #walrus operator
+# <<= (left shift operator)
+# >>= (right shift operator)
+# ^= (bitwise xor operator)
+# |= (bitwise or operator)
+# &= (bitwise and operator)
+
+# logical operator
+# and (returns true if both statements are true)
+# or  (returns true if one of the statements is true)
+# not (reverse the result, returns false if the result is true)
+
+# identity operators
+# is (returns true if the both variables are the same object)
+# is not (returns true if both variables are not the same object)
+
+# membership operator
+# in (returns true if a sequence with the specified value is present in the object)
+# not in (returns true if a sequence with the specified value is not present in the object)
+
+# bitwise operators
+# & - sets each bit to 1 if both bits are 1
+# | - sets each bit to 1 if one of two bits is 1
+# ^ - sets each bit to 1 if only one of two bits is 1
+# ~ - inverts all the bits
+# << - zero fill leftshift operator - 
+# >> - signed right shift operator -
+
+# operator precedence
+
+
+
+
+
+
+
